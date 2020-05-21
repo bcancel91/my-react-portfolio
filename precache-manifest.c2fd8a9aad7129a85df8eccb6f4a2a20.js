6da0a7194b86bad9be65409a456fa5ea38d39f62
Brian Cancel <brian.e.cancel@gmail.com>
2020-05-21 17:43:07 -0400

    Updates

---
+++ precache-manifest.c2fd8a9aad7129a85df8eccb6f4a2a20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e556db5dcc97fdec56ddbf1047d1047",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "c1155a99748e6aadf7c6",
+    "url": "/my-react-portfolio/static/css/main.ac52997d.chunk.css"
+  },
+  {
+    "revision": "6e7cd5b840549650f852",
+    "url": "/my-react-portfolio/static/js/2.b1438598.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-portfolio/static/js/2.b1438598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1155a99748e6aadf7c6",
+    "url": "/my-react-portfolio/static/js/main.a27398aa.chunk.js"
+  },
+  {
+    "revision": "b5445f86308fdf0e336c",
+    "url": "/my-react-portfolio/static/js/runtime-main.f638b4f6.js"
+  },
+  {
+    "revision": "f7492b1a74222395b35fe7ccf630b8d5",
+    "url": "/my-react-portfolio/static/media/WebDevelopmentResume.f7492b1a.pdf"
+  },
+  {
+    "revision": "8a493d79df2cc9bc34a9587097e0937c",
+    "url": "/my-react-portfolio/static/media/brianPic.8a493d79.JPG"
+  }
+]);
